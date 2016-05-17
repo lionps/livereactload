fcb75ee092624b738a4fb42c2ad0c023212a3ca1
Lion Burger <lion@ps-equity.com>
2016-05-17 08:03:59 +0300

    fix

--- src/client/main.js
+++ src/client/main.js
@@ -8,10 +8,11 @@ module.exports = function client(opts, start = startClient) {
   start(scope$$, {
     change(msg) {
       info("Bundle changed")
-      handleChange(scope$$, msg.data)
+      handleChanges(scope$$, msg.data)
     },
     module(msg) {
       info("Module change")
+      Object.assign(scope$$.modules[msg.data.id], msg.data);
       patchModule(scope$$, msg.data)
     },
     bundle_error(msg) {
